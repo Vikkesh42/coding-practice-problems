326bdd7ddfd00c13601cfa6ff13d23b20c817b9b
Vikkesh42 <vikkeshpsn42@gmail.com>
2025-04-10 10:50:15 +0530

    Add LeetCode: [product of array except self] [medium]

---
+++ medium/product of array except self.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/product-of-array-except-self/
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        arr=[1]*len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            arr[i] = prefix
+            prefix*=nums[i]
+
+
+        suffix=1
+        for i in range(len(nums)-1,-1,-1):
+            arr[i]*= suffix
+            suffix*=nums[i]
+
+
+        return arr
+        
