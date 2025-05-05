585de3a4597e42fc8116d72aebdaee6acaf41caf
Vikkesh42 <vikkeshpsn42@gmail.com>
2025-05-05 22:01:13 +0530

    add two sum problem

---
+++ easy/two sum.py
@@ -0,0 +1,10 @@
+#https://leetcode.com/problems/two-sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+
+        for i,num in enumerate(nums):
+            com = target - num
+            if com in hash:
+                return [hash[com],i]
+            hash[num] = i
