2eca187e3cd6a49247dc376f0347e8bfbf71c903
Vikkesh42 <vikkeshpsn42@gmail.com>
2025-06-23 12:41:28 +0530

    add the stack easy problem

---
+++ easy/valid paranthesis.py
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/valid-parentheses/
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pair = {')': '(', '}': '{', ']': '['}
+
+        for ch in s:
+            if ch in "({[":
+                stack.append(ch)
+            else:
+                if not stack or stack[-1] != pair[ch]:
+                    return False
+                stack.pop()
+
+        return not stack
